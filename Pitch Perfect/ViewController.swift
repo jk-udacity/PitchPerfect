f54a94abb107b24e29d2ac64dd06e1287ffe78c7
jk-udacity <jk-udacity@users.noreply.github.com>
2016-02-21 21:17:21 +0100

    Stop button is invisible by default, it shows up once the recording button has been pressed. Record button is now disabled when pressed and enabled when stop button is pressed.

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var microphoneButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,9 +22,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        stopButton.hidden = true
+    }
 
     @IBAction func recordAudio(sender: UIButton) {
         recordingLabel.hidden = false
+        stopButton.hidden = false
+        microphoneButton.enabled = false
         //TODO: Record the user's voice
         print("in recordAudio")
     }
@@ -32,6 +42,7 @@ class ViewController: UIViewController {
         //TODO: Stop recording user's voice
         print("in stopRecording")
         recordingLabel.hidden = true
+        microphoneButton.enabled = true
     }
 }
 
