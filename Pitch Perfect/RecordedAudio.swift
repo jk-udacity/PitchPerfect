549c0173286ce4bb1617ee4d231d8184a7202d7a
jk-udacity <jk-udacity@users.noreply.github.com>
2016-02-24 21:15:20 +0100

    Added new model class. Created custom seagway.

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Pitch Perfect
+//
+//  Created by Mirko Jurić-Kavelj on 2/24/16.
+//  Copyright © 2016 Mirko Jurić-Kavelj. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
